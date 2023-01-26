3884d1b26a400ca63c45799bf8e2a4803d9a0b32
NoiR_CCC <noir662607004@gmail.com>
2023-01-26 20:42:24 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # TimelineBlender
  A lightweight utility class for fading timelines
+
+# Usage
+A `TimelineBlender` instance provides the ability to fade in / fade out a playable director's animation output.
+
+### Fade in
+Set the desired `_playableDirector.time` to start at. The timeline automatically starts or resumes when it begins to fade in.
+```csharp
+timelineBlender.FadeIn(float duration)
+```
+When fading in a timeline for the first time, the timeline blender redirects the playable graph's output to a dummy output, so that it can control its weight. This will only happen once, as long as the playable graph is not destroyed (e.g. by `playableDirector.Stop()` or `playableDirector.Play()`)
+
+### Fade out
+```csharp
+timelineBlender.FadeOut(float duration)
+```
+When the weight reaches zero, the timeline will be paused. Then, a `timelineBlender.FadeOutCompleted` event will be invoked. You can listen to this event to trigger your own methods.
+
+### Note
+When fading out and back into the same timeline, having animation clips with root motion may cause the corresponding gameobject to incorrectly reset its position. In this case, it is recommended to set the fade duration to zero.
+
+<details>
+    <summary>Usage Example</summary>
+
+`MonoTimelineSkill` is a component attached to a skill prefab that has a playable director. A high-level skill controller maintains a collection of skill instances in the scene. 
+
+When a skill is played, the corresponding `MonoTimelineSkill` will activate its gameobject and fade in its timeline over time. 
+
+When a skill is stopped, the corresponding `MonoTimelineSkill` will fade out its timeline over time, and deactive its gameobject when the fade out completes.
+
+```csharp
+    /// <summary>
+    /// MonoBehaviour component that controls a timeline skill.
+    /// </summary>
+    [RequireComponent(typeof(PlayableDirector))]
+    public class MonoTimelineSkill : MonoSkill
+    {
+        [SerializeField] private PlayableDirector _playableDirector;
+
+        private TimelineBlender _timelineBlender;
+
+        #region MonoBehaviour
+        private void OnValidate()
+        {
+            if (_playableDirector == null) TryGetComponent(out _playableDirector);
+        }
+
+        private void Awake()
+        {
+            OnValidate();
+            _timelineBlender = new TimelineBlender(_playableDirector);
+        }
+
+        private void OnEnable()
+        {
+            _timelineBlender.FadeOutCompleted += OnFadeOutCompleted;
+        }
+
+        private void OnDisable()
+        {
+            _timelineBlender.FadeOutCompleted -= OnFadeOutCompleted;
+        }
+
+        private void Update()
+        {
+            _timelineBlender.OnUpdate(Time.deltaTime);
+        }
+        #endregion
+
+        public override void Play(float fadeInDuration = 0f)
+        {
+            this.gameObject.SetActive(true);
+            _playableDirector.time = 0f;
+            _timelineBlender.FadeIn(fadeInDuration);
+            _playableDirector.stopped += OnPlayableDirectorStopped;
+        }
+
+        public override void Stop(float fadeOutDuration = 0f)
+        {
+            _timelineBlender.FadeOut(fadeOutDuration);
+        }
+
+        private void OnFadeOutCompleted()
+        {
+            _playableDirector.stopped -= OnPlayableDirectorStopped;
+            this.gameObject.SetActive(false);
+        }
+
+        private void OnPlayableDirectorStopped(PlayableDirector playableDirector)
+        {
+            Complete();
+        }
+
+        /// <summary>
+        /// Setup timeline track bindings using the specified track name dictionary.
+        /// </summary>
+        /// <param name="bindingDict"></param>
+        public void SetupBindings(Dictionary<string, UnityEngine.Object> bindingDict)
+        {
+            TimelineAsset timelineAsset = (TimelineAsset)_playableDirector.playableAsset;
+            var outputTracks = timelineAsset.GetOutputTracks();
+            foreach (var track in outputTracks)
+            {
+                if (bindingDict.TryGetValue(track.name, out UnityEngine.Object obj))
+                {
+                    Debug.Log("[Mono Timeline Skill] Binding timeline track: " + track.name);
+                    _playableDirector.SetGenericBinding(track, obj);
+                }
+            }
+        }
+    }
+```
+</details>
+
+
---
+++ TimelineBlender.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace NekoNeko
+{
+    /// <summary>
+    /// Provides the ability to fade a playable director's animation output weight over time.
+    /// <para>Each <see cref="TimelineBlender"/> instance handles one playable director.</para>
+    /// </summary>
+    public class TimelineBlender
+    {
+        private enum FadeMode
+        {
+            In,
+            Out,
+        }
+
+        public AnimationPlayableOutput AnimationPlayableOutput => _output;
+
+        #region Fields
+        private AnimationPlayableOutput _output;
+        private bool _isFading;
+        private float _fadeSpeed;
+        #endregion
+
+        #region Events
+        public event Action FadeOutCompleted;
+        #endregion
+
+        #region Control
+        /// <summary>
+        /// Setup the playable director for fading.
+        /// </summary>
+        /// <param name="playableDirector"></param>
+        public void Setup(PlayableDirector playableDirector)
+        {
+            if (!playableDirector.playableGraph.IsValid())
+            {
+                playableDirector.RebuildGraph();
+                ConnectDummyOutput(playableDirector);
+            }
+        }
+
+        /// <summary>
+        /// Update tick.
+        /// </summary>
+        /// <param name="deltaTime"></param>
+        public void OnUpdate(float deltaTime)
+        {
+            UpdateFade(deltaTime);
+        }
+
+        /// <summary>
+        /// Start to fade in for the specified duration.
+        /// </summary>
+        /// <param name="duration"></param>
+        public void FadeIn(float duration)
+        {
+            StopFade();
+            if (duration == 0f)
+            {
+                SetWeight(0f);
+                StartFade(FadeMode.In, duration);
+                return;
+            }
+            else SetWeight(1f);
+        }
+
+        /// <summary>
+        /// Start to fade out for the specified duration.
+        /// <para><see cref="FadeOutCompleted"/> will be invoked when the weight becomes zero.</para>
+        /// </summary>
+        /// <param name="duration"></param>
+        public void FadeOut(float duration)
+        {
+            StopFade();
+            if (duration == 0f)
+            {
+                SetWeight(0f);
+                FadeOutCompleted?.Invoke();
+                return;
+            }
+            else StartFade(FadeMode.Out, duration);
+        }
+        #endregion
+
+        #region Fading
+        private void StartFade(FadeMode fadeMode, float duration)
+        {
+            switch (fadeMode)
+            {
+                case FadeMode.In:
+                    _fadeSpeed = 1f / duration;
+                    break;
+                case FadeMode.Out:
+                    _fadeSpeed = -1f / duration;
+                    break;
+            }
+            _isFading = true;
+        }
+
+        private void UpdateFade(float deltaTime)
+        {
+            if (!_isFading) return;
+
+            float weight = GetWeight();
+            weight += _fadeSpeed * deltaTime;
+            if (weight >= 1f)
+            {
+                // Fade in complete.
+                SetWeight(1f);
+                StopFade();
+                return;
+            }
+            else if (weight <= 0f)
+            {
+                // Fade out complete.
+                SetWeight(0f);
+                StopFade();
+                UnityEngine.Debug.Log("Fade out complete.");
+                FadeOutCompleted?.Invoke();
+                return;
+            }
+            else SetWeight(weight);
+        }
+
+        private void StopFade()
+        {
+            _isFading = false;
+            _fadeSpeed = 0f;
+        }
+        #endregion
+
+        #region Helpers
+        /// <summary>
+        /// Redirect the playable director graph's output to a new dummy output so we can control its weight.
+        /// </summary>
+        /// <param name="playableDirector"></param>
+        private void ConnectDummyOutput(PlayableDirector playableDirector)
+        {
+            if (!playableDirector.playableGraph.IsValid()) return;
+            var oldOutput = (AnimationPlayableOutput)playableDirector.
+                playableGraph.GetOutputByType<AnimationPlayableOutput>(0);
+
+            if (oldOutput.IsOutputValid() && oldOutput.GetTarget() != null)
+            {
+                _output = AnimationPlayableOutput.Create(
+                    playableDirector.playableGraph, "Dummy Output", oldOutput.GetTarget());
+                var sourcePlayable = oldOutput.GetSourcePlayable();
+                int outputPort = oldOutput.GetSourceOutputPort();
+                oldOutput.SetSourcePlayable(Playable.Null, -1);
+                _output.SetSourcePlayable(sourcePlayable, outputPort);
+                _output.SetWeight(1f);
+            }
+        }
+
+        public float GetWeight()
+        {
+            return _output.IsOutputValid() ? _output.GetWeight() : 0f;
+        }
+
+        public void SetWeight(float weight)
+        {
+            if (_output.IsOutputValid()) _output.SetWeight(weight);
+        }
+        #endregion
+    }
+}
